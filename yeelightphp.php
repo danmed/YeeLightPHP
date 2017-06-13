4fd155a5777934380780ea269594918c6d78117c
danmed <danmed@gmail.com>
2017-06-13 15:47:22 +0100

    Create yeelightphp.php

--- yeelightphp.php
+++ yeelightphp.php
@@ -1,23 +1,20 @@
 <?PHP  
 
-*/ Get Bulb IP and the action you want to perform /*
+//Get Bulb IP and the action you want to perform
   $bulb = $_GET['bulb'];
   $action = $_GET['action'];
 
-*/ Set transition and duration globally /*
+//Set transition and duration globally
   $effect = "smooth";
   $duration = "500";
 
-*/ Build command to send to yeecli /*
+//Build command to send to yeecli
   $command = 'yee --ip=' . $bulb . ' -e ' . $effect . ' -d ' . $duration . ' ' . $action;
 
-*/ Execute command /*
+//Execute command
   $output = shell_exec($command);
 
-*/ Echo the result /*
+//Echo the result
   echo "<pre>$output</pre>";
-
-
-
 
 ?>
