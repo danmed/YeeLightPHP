1e58a2b07837c4ff2e51ec1af10e6c30ed2bbc5b
danmed <danmed@gmail.com>
2017-06-13 15:41:18 +0100

    Create yeelightphp.php

--- yeelightphp.php
+++ yeelightphp.php
@@ -1,5 +1,17 @@
 <?php
 
+*/ Add bulbs to array below */
+*/ ("NAME","IP","EFFECT","DURATION") */
+*/ Effect can be smooth or instant */
+
+$bulbs = array
+  (
+  array("LAURA-LAMP","192.168.2.210","smooth","500"),
+  array("DAN-LAMP","192.168.2.211","smooth","500")
+  );
+  
+
+IF(ISSET($_GET['bulb'])) {
 */ Get Bulb IP and the action you want to perform */
   $bulb = $_GET['bulb'];
   $action = $_GET['action'];
@@ -16,5 +28,14 @@
 
 */ Echo the result */
   echo "<pre>$output</pre>";
+}
+
+for ($row = 0; $row < 4; $row++) {
+
+  for ($col = 0; $col < 4; $col++) {
+    echo "<li>".$bulbs[$row][$col]."</li>";
+  }
+
+}
 
 ?>
