35d5fb7452d95dfdd2878f9525e912ffc37042d8
danmed <danmed@gmail.com>
2017-06-13 15:20:28 +0100

    Create yeelightphp.php

--- yeelightphp.php
+++ yeelightphp.php
@@ -1,18 +1,41 @@
 <?php
+
+*/ Build array of bulbs as below */
+*/ ("NAME","IP","EFFECT","DURATION") */
+
+$bulbs = array
+  (
+  array("LAURA-LAMP","192.168.2.210","smooth","500"),
+  array("DAN-LAMP","192.168.2.211","smooth","500")
+  );
+
 */ Get Bulb IP and the action you want to perform */
-$bulb = $_GET['bulb'];
-$action = $_GET['action'];
+IF (ISSET($_GET['bulb']))
+  {
+  $bulb = $_GET['bulb'];
+  $action = $_GET['action'];
 
 */ Set transition and duration globally*/
-$effect = "smooth"
-$duration = "500"
+  $effect = "smooth"
+  $duration = "500"
 
 */ Build command to send to yeecli */
-$command = 'yee --ip=' . $bulb . ' -e ' . $effect . ' -d ' . $duration . ' ' . $action;
+  $command = 'yee --ip=' . $bulb . ' -e ' . $effect . ' -d ' . $duration . ' ' . $action;
 
 */ Execute command */
-$output = shell_exec($command);
+  $output = shell_exec($command);
 
 */ Echo the result */
-echo "<pre>$output</pre>";
+  echo "<pre>$output</pre>";
+  }
+for ($row = 0; $row < 4; $row++) {
+  for ($col = 0; $col < 4; $col++) {
+    echo "<li>".$bulbs[$row][$col]."</li>";
+  }
+}
+
+
+
+
+
 ?>
